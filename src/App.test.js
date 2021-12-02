c783df05487ea0d959d056d6f0f4bcc449bb65a7
Sonia Zaldana Calles <zaldanasonia@gmail.com>
2021-12-02 00:24:22 -0500

    Adding validation tests using jest

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-console.log('I am running a test!');
-test('renders \'I am running!\'', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/I am running!/i);
-  expect(linkElement).toBeInTheDocument();
-});
